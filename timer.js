6529dc6fe62ea216fe0b65a6e6403c194dc3a09d
Pawel Baran <pawel.baran.programista@gmail.com>
2020-08-25 22:53:54 +0200

    menu done

---
+++ timer.js
@@ -0,0 +1,13 @@
+function odliczanie()
+	{
+		var dzisiaj = new Date();
+		
+		var dzien = dzisiaj.getDate();
+		var miesiac = dzisiaj.getMonth()+1;
+		var rok = dzisiaj.getFullYear();
+		
+		document.getElementById("clock").innerHTML = 
+		 dzien+"."+miesiac+"."+rok;
+		 
+		 setTimeout("odliczanie()",1000);
+	}
